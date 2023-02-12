10fb1e0f9f1fc925d4e3d5b277adb450d3fd04c6
Saadad Jahan Khan <heysaadad@gmail.com>
2023-02-13 02:16:47 +0600

    Readme Deleted

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -74,17 +74,7 @@ class MainController extends Controller
         session()->put('order', $order);
         return redirect('success');
     }
-    
-//       public function basic_email() {
-//       $data = array('name'=>"Virat Gandhi");
-   
-//       Mail::send(['text'=>'mail'], $data, function($message) {
-//          $message->to('abc@gmail.com', 'Tutorials Point')->subject
-//             ('Laravel Basic Testing Mail');
-//          $message->from('xyz@gmail.com','Virat Gandhi');
-//       });
-//       echo "Basic Email Sent. Check your inbox.";
-//   }
+
 
     function success(){
         if (!session('order')){
